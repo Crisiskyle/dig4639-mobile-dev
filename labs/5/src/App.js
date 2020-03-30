c7bdf761e946c311cd4668cc503d19984aa90231
Kyle <43049771+Crisiskyle@users.noreply.github.com>
2020-03-29 20:55:49 -0400

    Yes

---
+++ labs/5/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+import Card from  './components/card/index.js';
+const API = 'https://api.weather.gov/gridpoints/MLB/25,69/forecast';
+
+
+class App extends React.Component {
+  state = {
+    weather: []    
+  }
+  fetchWeather() {
+    // shows where to get the information 
+    fetch(API)
+    // changes everything to JSON
+    .then(response => response.json())
+    // updates the weather
+    .then(data => {
+      let periods = data.properties.periods;
+      console.log(data)
+      this.setState({
+        weather: periods
+      });
+    }
+    );
+  }
+  componentDidMount() {
+    this.fetchWeather();
+  }
+  render() {
+    return(
+      <>{console.log(this.state.weather)}
+        {(this.state.weather).map(
+            (state, i) => 
+            <Card
+                key={i}
+                id={state.number}
+                title={state.name}
+                content={state.temperature}
+                unit={state.temperatureUnit}
+                description={state.detailedForecast}
+            ></Card>
+        )}
+      </>
+    )
+  }
+}
+
+export default App;
